82585370de308e9fa45f3c4ad1a30b69b27a03ba
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2024-08-18 20:40:40 +0300

    cli: Add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{arg, command, Command, ArgMatches};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = parse_args();
+    match matches.subcommand() {
+        Some(("init", sub_matches)) => init(sub_matches),
+        _ => unreachable!("Exhausted list of subcommands and subcommand _required prevents `None`"),
+    }
+}
+
+fn parse_args() -> ArgMatches {
+    command!()
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("init")
+                .about("Initialize the repo")
+                .arg(arg!([NAME])),
+        )
+        .get_matches()
+}
+
+fn init(args: &ArgMatches) {
+    println!("Hello, World!");
 }
