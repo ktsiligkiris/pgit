1083ddb9bdb45a0ec9c7596d50d7f4dc35b195a5
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2024-08-28 22:03:09 +0300

    hash-object: Save object

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,20 @@
+use sha1::{Digest, Sha1};
 use std::fs;
+use std::{fs::File, io::Write};
 
 pub const GIT_DIR: &str = ".pgit";
 
 pub fn init() -> std::io::Result<()> {
     fs::create_dir(GIT_DIR)?;
+    fs::create_dir(format!("{GIT_DIR}/objects"))?;
     Ok(())
+}
+
+pub fn hash_object(data: String) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(data.clone());
+    let oid = format!("{:X}", hasher.finalize());
+    let mut file = File::create(format!("{GIT_DIR}/objects/{oid}")).unwrap();
+    file.write_all(data.as_bytes()).unwrap();
+    oid
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{arg, command, ArgMatches, Command};
 use std::env;
+use std::fs;
 
 mod data;
 
@@ -7,6 +8,7 @@ fn main() {
     let matches = parse_args();
     match matches.subcommand() {
         Some(("init", sub_matches)) => init(sub_matches),
+        Some(("hash-object", sub_matches)) => hash_object(sub_matches),
         _ => unreachable!("Exhausted list of subcommands and subcommand _required prevents `None`"),
     }
 }
@@ -21,14 +23,25 @@ fn parse_args() -> ArgMatches {
                 .about("Initialize the repo")
                 .arg(arg!([NAME])),
         )
+        .subcommand(
+            Command::new("hash-object")
+                .about("Create a blob of an object")
+                .arg(arg!([FILE])),
+        )
         .get_matches()
 }
 
-fn init(args: &ArgMatches) {
+fn init(_args: &ArgMatches) {
     let _ = data::init(); // Ignore for now the possibility of an error
     println!(
         "Initialized empty pgit directory in {}/{}",
         env::current_dir().unwrap().display(),
         data::GIT_DIR
     )
+}
+
+fn hash_object(args: &ArgMatches) {
+    let filepath = args.get_one::<String>("FILE").unwrap();
+    let contents = fs::read_to_string(filepath).expect("Should have been able to read the file");
+    println!("{}", data::hash_object(contents));
 }
